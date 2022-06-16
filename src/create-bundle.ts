dc331d8bf3834a07c1838667568d971e0365ac2a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-06-15 21:49:29 -0400

    feat: Put together basic bundler.

--- src/create-bundle.ts
+++ src/create-bundle.ts
@@ -1,27 +1,111 @@
+import fs from "fs";
 import { Request, Response } from "express";
 
 interface BundleData {
+  center: [number, number];
+  popupField: string;
+  maxZoom: number;
   mapUuid: string;
+  bounds: {
+    northWest: {
+      lat: number;
+      lng: number;
+    };
+    southEast: {
+      lat: number;
+      lng: number;
+    };
+  };
   csvUuid?: string;
   places: Record<string, string | number>[];
 }
 
 const createBundle = (request: Request, response: Response) => {
-  console.log(`Got response: ${response.statusCode}`);
   try {
-    console.log(request.body);
-    const { mapUuid, csvUuid, places } = request.body as BundleData;
+    const data = request.body as BundleData;
+    const path = `public/uploads/${data.mapUuid}`;
 
-    const message = {
-      m: mapUuid,
-      c: csvUuid,
-      p: places,
-    };
+    //1. copy csv if it exists to tiles folder
+    //2. craete data.json
+    fs.writeFileSync(
+      [path, "data.json"].join("/"),
+      JSON.stringify(request.body, null, 2)
+    );
+
+    fs.writeFileSync(
+      [path, "data.js"].join("/"),
+      "const data = " + JSON.stringify(request.body, null, 2) + ";"
+    );
+
+    //3. generate boilerplate html
+    fs.writeFileSync([path, "index.html"].join("/"), generateHtmlString(data));
+
+    //4. zip everything up and give it to download.
+
+    const message = "hi. bundle created";
 
     response.send(message);
   } catch (error) {
     response.status(500).send(error);
   }
+};
+
+const generateHtmlString = (data: BundleData) => {
+  return `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Map for ${data.mapUuid}</title>
+    <style>
+      body {
+        padding: 0px;
+        margin: 0px;
+      }
+      #map {
+        height: 100vh;
+        width: 100vw;
+      }
+    </style>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+       integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+       crossorigin=""/>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+       integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+       crossorigin=""></script>
+    <script src="data.js"></script>
+    <script>
+      // Establish map
+      const bounds = L.latLngBounds(
+          [${data.bounds.northWest.lat}, ${data.bounds.northWest.lng}], 
+          [${data.bounds.southEast.lat}, ${data.bounds.southEast.lng}]
+        );
+      const map = L.map('map', {
+        CRS: L.CRS.Simple,
+        maxZoom: ${data.maxZoom},
+        bounds,
+        center: bounds.getCenter(),
+        zoom: 1,
+      });
+
+      // Add tiles.
+      L.tileLayer('tiles/{z}/{y}/{x}.png', {
+        attribution: '<a href="http://antirubbersheeter.moacir.com">Antirubbersheeter</a>'
+      }).addTo(map);
+
+      // Add markers.
+      const { places } = data;
+      for (const place of places) {
+        const marker = L.marker([place.antirubbersheeterLat, place.antirubbersheeterLng]).addTo(map);
+      }
+
+      // Fly to bounds
+      map.flyToBounds(bounds);
+    </script>
+  </body>
+</html>
+`;
 };
 
 export default createBundle;
